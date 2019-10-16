0b37c0de60600992fa740701c404d62a2adaaa38
Michael <13432849094@163.com>
2019-10-16 23:09:50 +0800

    添加fileinput 插件

--- file-upload-download/src/main/java/com/michael/file/WebMvcConfigurer.java
+++ file-upload-download/src/main/java/com/michael/file/WebMvcConfigurer.java
@@ -1,7 +1,9 @@
 package com.michael.file;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -13,5 +15,11 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
                 .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS")
                 .allowCredentials(false).maxAge(3600);
      }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+    }
 
 }
